1babfaae08de6e8a4f8f0671c7921f8d8e4da040
JoeyZ56 <jzazzi56@gmail.com>
2025-03-27 12:28:55 -0700

    created the fetch function and basic Material UI components for fetching recipe images as a link to the full recipe

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Box, Grid, Typography, Button, CircularProgress } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Typography,
+  Button,
+  CircularProgress,
+  CardMedia,
+} from "@mui/material";
 import HamburgerMenu from "../../components/hamburgerMenu";
 
 /*
@@ -8,7 +15,7 @@ array of food by category, category leads to a page with all the food in that ca
 */
 
 const Home = () => {
-  const [recipes, setRecipes] = useState();
+  const [recipes, setRecipes] = useState([]);
   const [likedRecipes, setLikedRecipes] = useState();
   const [categories, setCategories] = useState();
   const [search, setSearch] = useState();
@@ -17,19 +24,36 @@ const Home = () => {
   const [error, setError] = useState();
 
   //handle search
-  const handleSearch = () => {
-    setSearchResults(recipes.filter((recipe) => recipe.name === search));
-  };
+  // const handleSearch = () => {
+  //   setSearchResults(recipes.filter((recipe) => recipe.name === search));
+  // };
 
   //handle category
-  const handleFilterCategory = (category) => {
-    setSearchResults(recipes.filter((recipe) => recipe.catefory === category));
-  };
+  // const handleFilterCategory = (category) => {
+  //   setSearchResults(recipes.filter((recipe) => recipe.catefory === category));
+  // };
 
   //get all recipes
   useEffect(() => {
-    const fetchRecipes = async () => {};
+    const fetchRecipes = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:5003/api/recipes");
+
+        if (!res.ok) {
+          throw new Error("Error fetching recipes for the frontend");
+        }
+
+        const data = await res.json();
+        setRecipes(data);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchRecipes();
   }, []);
+
   return (
     <Box>
       <HamburgerMenu />
@@ -51,7 +75,30 @@ const Home = () => {
         >
           Recipes
         </Typography>
-        <Box>{}</Box>
+        {recipes.length === 0 ? (
+          <Box>
+            <Typography>
+              No recipes have been added yet. Be the first!
+            </Typography>
+          </Box>
+        ) : loading ? (
+          <Box>
+            <CircularProgress />
+          </Box>
+        ) : (
+          <Box>
+            {recipes.map((recipe) => (
+              <Link key={uid} to={`/recipes/${recipe._id}`}>
+                <CardMedia
+                  component="img"
+                  image={`data:${recipe.mimeType};base64,${recipe.image}`}
+                  alt={recipe.recipeName}
+                />
+              </Link>
+            ))}
+          </Box>
+        )}
+        {error && <Typography color="error">{error.message}</Typography>}
       </Box>
     </Box>
   );
